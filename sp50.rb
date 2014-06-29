39aea8fde8af0c0c2e48e5a8da7cd7273a31fab3
Adam Beguelin <adam@sensr.net>
2014-06-29 10:56:10 -0600

    compute the total value of a portfolio and the average value.

--- sp50.rb
+++ sp50.rb
@@ -9,12 +9,21 @@ class SP50
     # make the cost vary 1..50, the num shares is 100
     (1..50).each { |s| @pfolio << MiStock.new("SP#{s}", s,100, Time.now.strftime("%m-%d"), s)  }
   end
+
+  def total_value
+    @pfolio.reduce(0){|sum, holding| sum += (holding.price * holding.num_shares)}
+  end
+  
+  def target_value
+    self.total_value/@pfolio.size
+  end
   
 end
 
 begin
   
   s = SP50.new
-  s.pfolio.each { |s| puts "#{s}" }
+  puts s.total_value
+  puts s.target_value
 
 end
